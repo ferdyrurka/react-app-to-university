2459175d1729bab088bb18c2839037f77a7ae547
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2021-04-22 19:48:45 +0200

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,37 @@ import LeftMenu from "./component/LeftMenu/LeftMenu";
 import TopNav from "./component/TopNav/TopNav";
 import styled from "styled-components";
 import store from "./tools/store";
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
+import Test from "./component/NotFound/Test";
+import {Route, BrowserRouter as Router, Switch,} from 'react-router-dom';
+
+const Container = styled.div`
+  margin: 0 auto;
+`;
 
 function App() {
-    const Container = styled.div`
-      margin: 0 auto;
-    `;
-
     return (
         <Provider store={store}>
-            <main>
-                <TopNav/>
-                <Container className="container-lg">
-                    <div className="row row-cols-md-2 row-cols-1">
-                        <LeftMenu/>
-                        <div className="col">
-                            Lorem ipsum
-                        </div>
-                    </div>
-                </Container>
-            </main>
+            <Router>
+                <Switch>
+                <Route path="/test" exact component={Test} />
+
+                <Route path="/">
+                    <main>
+                        <TopNav/>
+                        <Container className="container-lg">
+                            <div className="row row-cols-md-2 row-cols-1">
+                                <LeftMenu/>
+                                <div className="col">
+                                    <Switch>
+                                    </Switch>
+                                </div>
+                            </div>
+                        </Container>
+                    </main>
+                </Route>
+                </Switch>
+            </Router>
         </Provider>
     );
 }
---
+++ src/component/LatestPublications/LatestPublications.tsx
@@ -0,0 +1,7 @@
+function LatestPublications() {
+    return (
+        <div></div>
+    );
+}
+
+export default LatestPublications;

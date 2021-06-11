59fee01b26aa33be1bcf2855d8278a0f979bf90a
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2021-06-11 20:34:08 +0200

    added resume the work section

--- src/containers/Workspace/Workspace.tsx
+++ src/containers/Workspace/Workspace.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Redirect, useParams } from "react-router-dom";
 import {Item, ITEMS} from "../../component/Workspaces/WorkspaceItems";
 import WorkspaceHeader from "../../component/Workspaces/Site/WorkspaceHeader";
+import Comments from "../../component/ResumeYourWork/Comments";
+import {fetchUsers} from "../../actions/UserAction";
+import {fetchLatestPhotos} from "../../actions/PhotoAction";
 
 function Workspace() {
-    let { workspaceSlug } = useParams<{workspaceSlug: string}>();
+    useEffect(() => {
+        fetchUsers();
+        fetchLatestPhotos();
+    }, [])
 
+    let { workspaceSlug } = useParams<{workspaceSlug: string}>();
     const workspace: Item | undefined = ITEMS.find((item: Item) => item.slug === workspaceSlug);
 
     if (workspace === undefined) {
@@ -15,6 +22,7 @@ function Workspace() {
     return (
         <main>
             <WorkspaceHeader workspace={workspace}/>
+            <Comments/>
         </main>
     );
 }
